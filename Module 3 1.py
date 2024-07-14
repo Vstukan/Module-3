2e1d0f224e9c02ec11bc33ab5400dedd70e6e98b
Vstukan <v.stukan14@gmail.com>
2024-07-14 11:42:40 +0300

    Add files via upload

---
+++ Module 3 1.py
@@ -0,0 +1,32 @@
+calls = 0
+def string_info():
+   string = input('Введите слово: ')
+   print('(', len(string), ", '", string.upper(), ", '", string.lower(), "'", ')')
+   global calls
+   calls = calls + 1
+
+
+def is_contains():
+    string = input('Введите ещё слово: ')
+    list_to_search = list(input('Введите слова для сравнения: '))
+    stringUP = string.upper()
+    str_to_search = ''.join(list_to_search)
+    str_to_searchUP = str_to_search.upper()
+    if stringUP in str_to_searchUP:
+        print(True)
+    else:
+       print(False)
+    global calls
+    calls = calls + 1
+
+
+def count_calls():
+    global calls
+    print(calls)
+
+
+string_info()
+string_info()
+is_contains()
+is_contains()
+count_calls()
